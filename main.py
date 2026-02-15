dcba3fcae53e95c695a59425bf4820799a5234e8
kostia-ssss <kostiantyn.tel@gmail.com>
2026-02-15 15:38:11 +0200

    v3.0

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from maze import maze
+import math
 import pygame
 pygame.init()
 
@@ -61,27 +62,34 @@ class Player(Sprite):
             if self.rect.colliderect(obstacle.rect):
                 self.rect.x, self.rect.y = old_x, old_y
                 break
-
+            
 class Bullet(Sprite):
-    def __init__(self, x, y, w, h, img, direction):
-        super().__init__(x, y, w, h, img)
-        self.direction = direction
-        self.speed = 10
-    
-    def update(self):
-        if self.direction == "up":
-            self.rect.y -= self.speed
-        elif self.direction == "down":
-            self.rect.y += self.speed
-        elif self.direction == "left":
-            self.rect.x -= self.speed
-        elif self.direction == "right":
-            self.rect.x += self.speed
-        
-        if (self.rect.bottom < 0 or self.rect.top > H or 
-            self.rect.right < 0 or self.rect.left > W):
-            return True
-        return False
+    def __init__(self, x, y, w, h, image, speed, target_pos):
+        super().__init__(x, y, w, h, image)
+
+        self.speed = speed
+        self.pos = pygame.Vector2(self.rect.center)
+
+        direction = pygame.Vector2(target_pos) - self.pos
+
+        if direction.length() == 0:
+            direction = pygame.Vector2(1, 0)
+
+        self.direction = direction.normalize()
+
+        self.alive = True  # 👈 флаг життя
+
+    def move(self):
+        self.pos += self.direction * self.speed
+        self.rect.center = self.pos
+
+        if (
+            self.rect.right < 0 or
+            self.rect.left > map_width or
+            self.rect.bottom < 0 or
+            self.rect.top > map_height
+        ):
+            self.alive = False
 
 class Camera:
     def __init__(self, right_edge, left_edge, top_edge, bottom_edge, speed):
@@ -100,6 +108,7 @@ class Camera:
 
 font = pygame.font.SysFont("Century Gothic", 20, True)
 
+bullet_img = pygame.image.load("images/Bullet.png")
 menu_bg = Sprite(0, 0, W, H, pygame.image.load("images/BG.png"))
 game_bg = Sprite(0, 0, map_width, map_height, pygame.image.load("images/GameBG.png"))
 play_button = Sprite(W/2-75, H/2-35, 150, 70, pygame.image.load("images/Play.png"))
@@ -107,6 +116,7 @@ player = Player(100, 100, 50, 50, pygame.image.load("images/Player.png"), 5)
 camera = Camera(0.9, 0.9, 0.9, 0.9, player.speed)
 obstacles = []
 tiles = []
+bullets = []
 
 for row in maze:
     for char in row:
@@ -125,6 +135,7 @@ menu = True
 while running:
     window.fill((0, 0, 0))
     for event in pygame.event.get():
+        keys = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -133,11 +144,26 @@ while running:
             print(y)
             if play_button.rect.collidepoint(x, y):
                 menu = False
+        if keys[pygame.K_SPACE]:
+            mouse_pos = pygame.mouse.get_pos()
+            world_mouse = (
+                mouse_pos[0] - camera.x,
+                mouse_pos[1] - camera.y
+            )
+
+            bullets.append(Bullet(
+                player.rect.centerx,
+                player.rect.centery,
+                10, 10,
+                bullet_img,
+                8,
+                world_mouse
+            ))
     
     if menu:
         menu_bg.draw()
         play_button.draw()
-        version_txt = font.render("V0.2", True, (0, 0, 0))
+        version_txt = font.render("V0.3", True, (0, 0, 0))
         window.blit(version_txt, (0, 0))
     else:
         game_bg.draw()
@@ -149,6 +175,14 @@ while running:
             tile.draw()
             if player.rect.colliderect(tile.rect):
                 tile.effect(player)
+        
+        for bullet in bullets:
+            bullet.draw()
+            bullet.move()
+
+        # 🔥 Очищення після руху
+        bullets = [b for b in bullets if b.alive]
+
         
         player.update(obstacles)
         player.draw()
